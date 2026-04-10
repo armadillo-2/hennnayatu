99e714e23ce26f13263b6baa58755e34dc032558
Your Name <you@example.com>
2026-04-10 19:26:32 +0900

    終わり

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,16 @@
 let button = document.getElementById("button");
 button.addEventListener("click",function(){
     button.innerText = Number(button.innerText) + 1;
-    if (Number(button.innerText) >= 30){
+    if (Number(button.innerText) == 30){
+        document.body.style.backgroundColor = "#40ff40";
         alert("意味ないよ")
+    }
+    if (Number(button.innerText) == 100){
+        document.body.style.backgroundColor = "#ff00c8";
+        alert("意味ないって")
+    }
+    if (Number(button.innerText) == 300){
+        document.body.style.backgroundColor = "#252961";
+        alert("もうないよ")
     }
 })
